e199377b12e41fa088e7d2681f0393524145240e
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-08-07 21:31:12 +0200

    wip

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.myapplication
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +18,89 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
+/*
+~/Android/Sdk/platform-tools/adb devices -l
+~/Android/Sdk/platform-tools/adb -s adb-100000002e70bd0a-yn6lUH._adb-tls-connect._tcp shell
+su
+ps -A -f | grep wpa
+
+find | grep wpa_supplicant
+./vendor/etc/wifi/wpa_supplicant_overlay.conf # read only
+./vendor/etc/wifi/wpa_supplicant.conf # read only
+./data/vendor/wifi/wpa/wpa_supplicant.conf
+
+wpa_cli
+scan
+scan_results
+save_config
+dump
+driver_flags
+status verbose
+
+get p2p_disabled
+save_config
+dump
+set p2p_disabled 0
+dump
+save_config
+
+cat ./data/vendor/wifi/wpa/wpa_supplicant.conf
+
+cat << EOF > /data/vendor/wifi/wpa/wpa_supplicant.conf
+ctrl_interface=/data/vendor/wifi/wpa/sockets
+disable_scan_offload=1
+update_config=1
+p2p_add_cli_chan=1
+filter_rssi=-75
+pmf=1
+sae_pwe=2
+external_sim=1
+wowlan_triggers=any
+gas_rand_addr_lifetime=0
+gas_rand_mac_addr=1
+p2p_disabled=0
+
+network={
+        ssid="MagentaWLAN-L6J9"
+        psk="07764007550070636230"
+        key_mgmt=WPA-PSK FT-PSK WPA-PSK-SHA256
+        group=CCMP TKIP
+        id_str="%7B%22configKey%22%3A%22%5C%22MagentaWLAN-L6J9%5C%22WPA_PSK%22%2C%22creatorUid%22%3A%221000%22%7D"
+}
+EOF
+
+set p2p_disabled 0
+help p2p_find
+
+logcat wpa_supplicant:V *:S
+
+08-07 18:53:56.966  4420  4420 D wpa_supplicant: wlan0: P2P: Reject p2p_find operation (P2P disabled)
+
+/data/vendor/wifi/wpa/wpa_supplicant.conf
+
+-07 18:47:32.935  4420  4420 D wpa_supplicant: nl80211: Use separate P2P group interface (driver advertised support)
+08-07 18:47:32.935  4420  4420 D wpa_supplicant: nl80211: Enable multi-channel concurrent (driver advertised support)
+08-07 18:47:32.935  4420  4420 D wpa_supplicant: nl80211: use P2P_DEVICE support
+
+08-07 18:47:32.944  4420  4420 I wpa_supplicant: rfkill: Cannot open RFKILL control device
+08-07 18:47:32.944  4420  4420 D wpa_supplicant: nl80211: RFKILL status not available
+
+08-07 18:51:28.153  4420  4420 D wpa_supplicant: wlan0: Control interface command 'P2P_FIND'
+08-07 18:51:28.153  4420  4420 D wpa_supplicant: wlan0: P2P: Use 500 ms search delay due to concurrent operation
+08-07 18:51:28.153  4420  4420 D wpa_supplicant: wlan0: P2P: Reject p2p_find operation (P2P disabled)
+
+grep -rI --color "global->p2p ="
+wpas_p2p_init()
+
+separate p2p interface?
+
+iw list
+iw phy phy0 info
+iw phy pyh0 interface add p2p0 type p2p-device
+iw wdev <idx> p2p start
+ */
+private const val TAG = "MainActivity"
 
 // https://developer.android.com/jetpack/compose/tutorial
 // https://developer.android.com/training/data-storage/room
@@ -29,8 +117,31 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 // faq What is the relationship between Wi-Fi Aware and Wi-Fi Direct?
 // Wi-Fi Aware is a similar peer-to-peer connectivity technology to Wi-Fi Direct. However, while Wi-Fi Direct requires a centralized coordinator, called a Group Owner, Wi-Fi Aware creates decentralized, dynamic peer-to-peer connections. Many applications, such as Miracast and direct printer connections, work well with Wi-Fi Direct. Wi-Fi Aware is positioned to provide peer-to-peer connectivity in highly mobile environments, where devices join or leave in a less deterministic manner. Whether it's professionals at a crowded conference to find each other or strangers on a subway momentarily joining a multi-player game, Wi-Fi Aware connections seamlessly adapt to changing environment and usage conditions.
 class MainActivity : ComponentActivity() {
+    private val manager: WifiP2pManager? by lazy(LazyThreadSafetyMode.NONE) {
+        getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager?
+    }
+
+    private var receiver: BroadcastReceiver? = null
+
+    private val intentFilter = IntentFilter().apply {
+        addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+        addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (manager == null) {
+            Log.i(TAG, "Wifi P2P service not supported")
+        }
+
+        manager?.let {
+            val c = it.initialize(this, mainLooper, null)
+            receiver = WiFiDirectBroadcastReceiver(it, c, this)
+        }
+
         setContent {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
@@ -43,6 +154,23 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    /* register the broadcast receiver with the intent values to be matched */
+    override fun onResume() {
+        super.onResume()
+        receiver?.also { receiver ->
+            registerReceiver(receiver, intentFilter)
+        }
+    }
+
+    /* unregister the broadcast receiver */
+    override fun onPause() {
+        super.onPause()
+        receiver?.also { receiver ->
+            unregisterReceiver(receiver)
+        }
+    }
+
 }
 
 @Preview
---
+++ app/src/main/java/com/example/myapplication/WiFiDirectBroadcastReceiver.kt
@@ -0,0 +1,46 @@
+package com.example.myapplication
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.wifi.p2p.WifiP2pManager
+import android.util.Log
+
+private const val TAG = "WiFiDirectBroadcastReceiver"
+
+class WiFiDirectBroadcastReceiver(
+    private val manager: WifiP2pManager,
+    private val channel: WifiP2pManager.Channel,
+    private val activity: MainActivity
+) : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                when (intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)) {
+                    WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
+                        // Wifi P2P is enabled
+                        Log.i(TAG, "Wifi P2P is enabled")
+                    }
+
+                    else -> {
+                        // Wi-Fi P2P is not enabled
+                        Log.i(TAG, "Wifi P2P is disabled")
+                    }
+                }
+            }
+
+            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+                // Call WifiP2pManager.requestPeers() to get a list of current peers
+            }
+
+            WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+                // Respond to new connection or disconnections
+            }
+
+            WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+                // Respond to this device's wifi state changing
+            }
+        }
+
+    }
+}
